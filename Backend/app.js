45ff7eddcf9036f196e7ec734024889a1a58df60
Felipe_alves518 <brisket.bucket@gmail.com>
2025-11-18 15:31:09 -0300

    prepared for deployment

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,9 @@ const express = require('express');
 const app = express();
 const connectDb = require('./db/connect');
 const cors = require('cors');
+const path = require('path')
+
+const _dirname = path.resolve()
 
 
 const errorHandlerMiddleware = require('./Middleware/error-handler')
@@ -19,6 +22,15 @@ app.use(errorHandlerMiddleware);
 //routes
 const products = require('./routes/products');
 app.use("/api/v1/products", products);
+
+
+//frontend
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(_dirname,"../frontend/dist")))
+    app.get("*splat",(req,res) => {
+    res.sendFile(path.join(_dirname,"../frontend","dist","index.html"))
+})
+}
 
 const PORT = process.env.PORT || 5000;
 
